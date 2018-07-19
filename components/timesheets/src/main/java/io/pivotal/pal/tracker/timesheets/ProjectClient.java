09cc19355d7ea971ca1968a0cc2390de958e2834
Nilesh Miskin <nilesh.262482@ad.infosys.com>
2018-07-19 13:48:23 +0530

    Done but it didn't show closed circuit.

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,18 +1,36 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
+    private final Logger logger = LoggerFactory.getLogger(ProjectClient.class);
     private final RestOperations restOperations;
     private final String endpoint;
+    private final Map<Long, ProjectInfo> cache;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
+        cache = new ConcurrentHashMap<>();
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        logger.info("###### Calling getProject - original method");
+        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        cache.putIfAbsent(projectId, projectInfo);
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId){
+        logger.info("###### Calling getProjectFromCache - cache method");
+        return cache.get(projectId);
     }
 }
