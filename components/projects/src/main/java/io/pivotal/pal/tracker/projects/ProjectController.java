0e6e00912ea8d0539011f1e02035da3119d15480
Nilesh Miskin <nilesh.262482@ad.infosys.com>
2018-07-19 11:48:38 +0530

    Done and dusted

--- components/projects/src/main/java/io/pivotal/pal/tracker/projects/ProjectController.java
+++ components/projects/src/main/java/io/pivotal/pal/tracker/projects/ProjectController.java
@@ -3,6 +3,9 @@ package io.pivotal.pal.tracker.projects;
 import io.pivotal.pal.tracker.projects.data.ProjectDataGateway;
 import io.pivotal.pal.tracker.projects.data.ProjectFields;
 import io.pivotal.pal.tracker.projects.data.ProjectRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +20,15 @@ import static java.util.stream.Collectors.toList;
 @RequestMapping("/projects")
 public class ProjectController {
 
-    private final ProjectDataGateway gateway;
+    private final Logger log = LoggerFactory.getLogger(ProjectController.class);
 
-    public ProjectController(ProjectDataGateway gateway) {
+    private final ProjectDataGateway gateway;
+    private final long delayMillis;
+
+    public ProjectController(ProjectDataGateway gateway,
+                             @Value("${delay.millis:0}") long delayMillis) {
         this.gateway = gateway;
+        this.delayMillis = delayMillis;
     }
 
     @PostMapping
@@ -32,9 +40,9 @@ public class ProjectController {
     @GetMapping
     public List<ProjectInfo> list(@RequestParam long accountId) {
         return gateway.findAllByAccountId(accountId)
-            .stream()
-            .map(this::present)
-            .collect(toList());
+                .stream()
+                .map(this::present)
+                .collect(toList());
     }
 
     @GetMapping("/{projectId}")
@@ -42,6 +50,16 @@ public class ProjectController {
         ProjectRecord record = gateway.find(projectId);
 
         if (record != null) {
+            log.info("returning project info for project id {}", projectId);
+
+            if (delayMillis > 0) {
+                log.info("artificial delay of {} ms", delayMillis);
+                try {
+                    Thread.sleep(delayMillis);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
             return present(record);
         }
 
@@ -51,19 +69,19 @@ public class ProjectController {
 
     private ProjectFields formToFields(ProjectForm form) {
         return projectFieldsBuilder()
-            .accountId(form.accountId)
-            .name(form.name)
-            .active(form.active)
-            .build();
+                .accountId(form.accountId)
+                .name(form.name)
+                .active(form.active)
+                .build();
     }
 
     private ProjectInfo present(ProjectRecord record) {
         return projectInfoBuilder()
-            .id(record.id)
-            .accountId(record.accountId)
-            .name(record.name)
-            .active(record.active)
-            .info("project info")
-            .build();
+                .id(record.id)
+                .accountId(record.accountId)
+                .name(record.name)
+                .active(record.active)
+                .info("project info")
+                .build();
     }
 }
